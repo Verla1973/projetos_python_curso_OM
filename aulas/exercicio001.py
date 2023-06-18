698d9af6675c3f8e12f95ba1be300d9538cf4ed0
eskurinho <manoads2019@gmail.com>
2023-06-18 15:11:34 -0300

    exercicio001

---
+++ aulas/exercicio001.py
@@ -0,0 +1,10 @@
+primeiro_valor = float(input('Digite o primeiro valor: '))
+segundo_valor = float(input('Digite o segundo valor: '))
+
+
+if (primeiro_valor > segundo_valor):
+    print(f'O primeiro valor é maior que o segundo!!!')
+elif (primeiro_valor == segundo_valor):
+    print(f'Os valores digitados possuem o mesmo valor!!!')
+else:
+    print(f'O primeiro valor é menor que o segundo!!!!')
